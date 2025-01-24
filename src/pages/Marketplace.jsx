6d5368b11b4b1427fb045a2dcd59aa973ff734b7
tincleo <tincleo@gmail.com>
2025-01-24 22:50:13 +0100

    Two products per row on mobile

--- src/pages/Marketplace.jsx
+++ src/pages/Marketplace.jsx
@@ -149,7 +149,7 @@ function Marketplace() {
           onChange={handleChange}
         />
 
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-2 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {products.map(product => (
             <ProductCard
               key={product.id}
